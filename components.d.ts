9ebe60045ef07ed68618cf4a6af88b0ae1a6eaa5
cnaegele <cnaegele@lausanne.ch>
2025-10-23 17:17:34 +0200

    Types affaire, droit creation par unite developpement en cours

--- components.d.ts
+++ components.d.ts
@@ -11,6 +11,8 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
+    CallerInfo: typeof import('./src/components/CallerInfo.vue')['default']
+    CallerIsInGroup: typeof import('./src/components/CallerIsInGroup.vue')['default']
     TypesAffaireListeChoix: typeof import('./src/components/TypesAffaireListeChoix.vue')['default']
     UniteOrgChoix: typeof import('./src/components/UniteOrgChoix.vue')['default']
     UniteOrgNode: typeof import('./src/components/UniteOrgNode.vue')['default']
